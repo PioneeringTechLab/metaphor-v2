301f8440d8e3eea19f431b3ef1ea9d72626ff511
luisjg <contact@luisjg.dev>
2024-03-20 22:48:36 -0700

    Adjust once again the deploy branch.

--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,18 @@ set -e
 # build
 npm run docs:build
 
-# navigate into the build output directory
-#cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# Add the built assets to the master branch
+git add docs
+git commit -m "Push out a new update."
+git push
 
+# create a new version of the gh-pages branch
 git subtree split --branch gh-pages --prefix docs/.vuepress/dist/
+
+# switch to newly created branch and push
 git checkout gh-pages
 git push
+
+# go back to master branch
 git checkout -
-git rm gh-pages
-#git init
-#git add -A
-#git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f https://github.com/PioneeringTechLab/metaphor-v2.git master:gh-pages
-
-#cd -
