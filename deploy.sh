43e5c095318f7ba282fb9d0de77b13e0e742d925
luisjg <contact@luisjg.dev>
2024-03-20 23:17:10 -0700

    Fix the script again.

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ npm run docs:build
 
 # create a new version of the gh-pages branch
 git subtree split --branch gh-pages --prefix docs/.vuepress/dist/ master
-git subtree push
+#git subtree push
 # switch to newly created branch and push
-#git checkout gh-pages
-#git push
+git checkout gh-pages
+git push
 
 # go back to master branch
 #git checkout -
