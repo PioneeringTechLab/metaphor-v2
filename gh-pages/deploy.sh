1f408c0ad6cd26565edf97fcaa937f90e40c5a6e
luisjg <contact@luisjg.dev>
2024-03-20 23:09:40 -0700

    Add 'gh-pages/' from commit 'f4776e33f5d5d1a573a5227f5cde8761a9d98917'
    git-subtree-dir: gh-pages
    git-subtree-mainline: f4776e33f5d5d1a573a5227f5cde8761a9d98917
    git-subtree-split: f4776e33f5d5d1a573a5227f5cde8761a9d98917

---
+++ gh-pages/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+
+# create a new version of the gh-pages branch
+git subtree split --branch gh-pages --prefix docs/.vuepress/dist/
+git subtree push
+# switch to newly created branch and push
+#git checkout gh-pages
+#git push
+
+# go back to master branch
+#git checkout -
---
+++ gh-pages/docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    title: 'Metaphor v2',
+    description: 'A CSUN-Branded Theme for Bootstrap 4',
+    base: './metaphor-v2/',
+    head: [
+        ['script', { src: 'https://code.jquery.com/jquery-3.3.1.min.js' }],
+        ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' }],
+        ['script', { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' }],
+        ['script', { src: 'https://pioneeringtechlab.net/metaphor/dist/js/datepicker/datepicker.js' }]
+    ],
+    themeConfig: {
+        search: false,
+        repo: 'PioneeringTechLab/metaphor-v2',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Getting Started', link: '/getting-started/' },
+            { text: 'Documentation', link: '/documentation/' }
+        ],
+        sidebar: [
+            ['/', 'Home'],
+            ['/getting-started/', 'Getting Started'],
+            ['/documentation/', 'Documentation']
+        ] 
+    }
+}
